e86b2ba35febf1f2cf401443811d5858efc3266c
Muhammad Jindan <jindanza7@gmail.com>
2024-11-28 16:21:39 +0700

    feat[main]: simple rest api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Item {
+    id: u32,
+    name: String,
+}
+
+static mut ITEMS: Vec<Item> = vec![];
+
+#[get("/items")]
+async fn get_items() -> impl Responder {
+    unsafe { web::Json(&ITEMS) }
+}
+
+#[post("/items")]
+async fn create_items(item: web::Json<Item>) -> impl Responder {
+    unsafe {
+        ITEMS.push(item.into_inner());
+        "Item Added"
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(get_items).service(create_items)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
